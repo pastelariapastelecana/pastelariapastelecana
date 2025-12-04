b3c034c9e114efb042ee810fde1fc3cbaf828fdb
pastelariapastelecana <pastelariapastelecana@gmail.com>
2025-12-04 14:01:17 -0300

    Delete backend/src/routes/webhookRoutes.js

--- backend/src/routes/webhookRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleMercadoPagoWebhook } = require('../controllers/webhookController');
-
-router.post('/mercadopago', handleMercadoPagoWebhook);
-
-module.exports = router;
