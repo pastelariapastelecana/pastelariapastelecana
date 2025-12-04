d655f96d3181a4b94dc9912a9e37067fe692a37c
pastelariapastelecana <pastelariapastelecana@gmail.com>
2025-12-04 15:26:39 -0300

    Delete backend/src/routes/pixRoutes.js

--- backend/src/routes/pixRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generatePix } = require('../controllers/pixController');
-
-router.post('/generate', generatePix);
-
-module.exports = router;
