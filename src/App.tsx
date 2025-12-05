0c3653ee79fa15dac37a74427b14a2de91048de7
pastelariapastelecana <pastelariapastelecana@gmail.com>
2025-12-05 13:31:07 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "@/contexts/CartContext";
-import { StoreStatusProvider } from "@/contexts/StoreStatusContext"; // Importar o Provider
+import { StoreStatusProvider } from "@/contexts/StoreStatusContext";
+import { useMercadoPago } from "@/hooks/use-mercado-pago"; // Importar o novo hook
 import Index from "./pages/Index";
 import Cardapio from "./pages/Cardapio";
 import CoffeeBreak from "./pages/CoffeeBreak";
@@ -21,35 +22,43 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 const queryClient = new QueryClient();
 
+// Componente wrapper para inicializar o Mercado Pago
+const AppWrapper = () => {
+  useMercadoPago(); // Inicializa o SDK globalmente
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/cardapio" element={<Cardapio />} />
+        <Route path="/coffee-break" element={<CoffeeBreak />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/cadastro" element={<Registration />} />
+        <Route path="/nossa-loja" element={<NossaLoja />} />
+        <Route path="/carrinho" element={<Carrinho />} />
+        <Route path="/checkout" element={<Checkout />} />
+        
+        {/* Rotas de Administração */}
+        <Route path="/admin/login" element={<Login />} />
+        <Route element={<ProtectedRoute redirectPath="/admin/login" />}>
+          <Route path="/admin/status" element={<StoreStatusAdmin />} />
+        </Route>
+
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <StoreStatusProvider> {/* Envolve o aplicativo com o provedor de status */}
+    <StoreStatusProvider>
       <CartProvider>
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/cardapio" element={<Cardapio />} />
-              <Route path="/coffee-break" element={<CoffeeBreak />} />
-              <Route path="/feedback" element={<Feedback />} />
-              <Route path="/contato" element={<Contato />} />
-              <Route path="/cadastro" element={<Registration />} />
-              <Route path="/nossa-loja" element={<NossaLoja />} />
-              <Route path="/carrinho" element={<Carrinho />} />
-              <Route path="/checkout" element={<Checkout />} />
-              
-              {/* Rotas de Administração */}
-              <Route path="/admin/login" element={<Login />} />
-              <Route element={<ProtectedRoute redirectPath="/admin/login" />}>
-                <Route path="/admin/status" element={<StoreStatusAdmin />} />
-              </Route>
-
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
+          <AppWrapper />
         </TooltipProvider>
       </CartProvider>
     </StoreStatusProvider>
