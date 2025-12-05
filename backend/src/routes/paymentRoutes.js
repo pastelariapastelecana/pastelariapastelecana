fe052a667cc1fc271ba74286f937d319777f4a07
pastelariapastelecana <pastelariapastelecana@gmail.com>
2025-12-04 21:19:49 -0300

    Update paymentRoutes.js

--- backend/src/routes/paymentRoutes.js
+++ backend/src/routes/paymentRoutes.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const { processPayment } = require('../controllers/paymentController');
+const { generatePix } = require('../controllers/pixController'); // Importar o controlador PIX
 
 router.post('/create-payment', processPayment);
+router.post('/generate-pix', generatePix); // Rota para gerar PIX Transparente
 
 module.exports = router;
